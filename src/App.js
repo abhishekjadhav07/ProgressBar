1a6008bf90c3acddceaec259f3c7e40e69a0d6b4
Abhishek Jadhav <63790209+abhishekjadhav07@users.noreply.github.com>
2023-08-24 09:59:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import ProgressBar from "./components/ProgressBar";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, []);
+  return (
+    <div className="App">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} />
+    </div>
+  );
+}
